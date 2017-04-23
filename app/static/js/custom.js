ab4c5483bc67a1a76c1144ed1107047b280d3b36
jkao1 <jkao1@stuy.edu>
2017-04-23 08:17:25 -0400

    fixed works. nav still needs to center & border

--- app/static/js/custom.js
+++ app/static/js/custom.js
@@ -1,6 +1,29 @@
 $(document).ready(function() {
 
     $(".navbar-toggle").click(function(){
+
+    });
+
+    var fixmeTop = $('.fixme').offset().top;
+
+    $(window).scroll(function() {
+
+        var currentScroll = $(window).scrollTop();
+        if (currentScroll > fixmeTop) {
+            $('.fixme').css({
+                position: 'fixed',
+                top: 0,
+                margin: '0 auto',
+            });
+            // fixed elements don't affect the layout of other elements.
+            // have to displace the element below to fake smooth transition when navbar collapses.
+            $('.content').css('margin-top', $('.logo-container').height() + 10);
+        } else {
+            $('.fixme').css({
+                position: 'static'
+            });
+            $('.content').css('margin-top', 0);
+        }
     });
 
 });
--- app/views.py
+++ app/views.py
@@ -28,6 +28,30 @@ def index():
             'body': 'Stuyvesant High School provides excellent education.',
             'viewcount': 10,
             'tags': [9, 10, 0, 34, 2]
+        },
+        {
+            'timestamp': '2017-04-15 09:49:03',
+            'articleID': 109,
+            'title': 'Stuyvesant!',
+            'authors': [
+                {'name': 'Sabrina Wen'},
+                {'name': 'Angela Tom'},
+            ],
+            'body': 'Stuyvesant High School provides excellent education.',
+            'viewcount': 10,
+            'tags': [9, 10, 0, 34, 2]
+        },
+        {
+            'timestamp': '2017-04-15 09:49:03',
+            'articleID': 109,
+            'title': 'Stuyvesant!',
+            'authors': [
+                {'name': 'Sabrina Wen'},
+                {'name': 'Angela Tom'},
+            ],
+            'body': 'Stuyvesant High School provides excellent education.',
+            'viewcount': 10,
+            'tags': [9, 10, 0, 34, 2]
         }
     ]
     return render_template("index.html",
