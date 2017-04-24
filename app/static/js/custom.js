5dfb5fbbbe17296bc45a23b83ad870c875a97a2e
jkao1 <jkao1@stuy.edu>
2017-04-24 16:19:14 -0400

    nav looks good

--- app/static/js/custom.js
+++ app/static/js/custom.js
@@ -10,18 +10,12 @@ $(document).ready(function() {
 
         var currentScroll = $(window).scrollTop();
         if (currentScroll > fixmeTop) {
-            $('.fixme').css({
-                position: 'fixed',
-                top: 0,
-                margin: '0 auto',
-            });
+            $('.fixme').addClass('fixed-topnav');
             // fixed elements don't affect the layout of other elements.
             // have to displace the element below to fake smooth transition when navbar collapses.
             $('.content').css('margin-top', $('.logo-container').height() + 10);
         } else {
-            $('.fixme').css({
-                position: 'static'
-            });
+            $('.fixme').removeClass('fixed-topnav');
             $('.content').css('margin-top', 0);
         }
     });
