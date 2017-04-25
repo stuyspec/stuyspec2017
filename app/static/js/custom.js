d9a0b9f9ac66fe6e73dd3dd0cd08b08af1c4f3c4
jkao1 <jkao1@stuy.edu>
2017-04-24 20:03:54 -0400

    fixed vertical collapse on navbar

--- app/static/js/custom.js
+++ app/static/js/custom.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
             $('.fixme').addClass('fixed-topnav');
             // fixed elements don't affect the layout of other elements.
             // have to displace the element below to fake smooth transition when navbar collapses.
-            $('.content').css('margin-top', $('.logo-container').height() + 150);
+            $('.content').css('margin-top', $('.logo-container').height() + 10);
         } else {
             $('.fixme').removeClass('fixed-topnav');
             $('.content').css('margin-top', 0);
